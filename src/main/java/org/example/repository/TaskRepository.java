b434edcd09bf60aea92352ba426676c60035dc99
otabekXub <ohub7405@gmail.com>
2025-01-28 18:47:48 +0500

    receiveEmployeeDeleteMessage

--- src/main/java/org/example/repository/TaskRepository.java
+++ src/main/java/org/example/repository/TaskRepository.java
@@ -2,12 +2,13 @@ package org.example.repository;
 
 import org.example.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
-    List<Task> findByEmployeeId(Long employeeId);
+    @Query("update Task t set t.deleted = true  where t.employeeId=:employeeId")
+    void deleteAllByEmployeeId(@Param("employeeId") Long employeeId);
 }
